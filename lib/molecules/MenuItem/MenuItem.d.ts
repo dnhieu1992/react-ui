1b4e516c9de9433f3992b8c85f7712985b55b828
dnhieu1992 <dnhieu1992@gmail.com>
2024-04-27 10:59:44 +0700

    Fix export MenuItem

--- lib/molecules/MenuItem/MenuItem.d.ts
+++ lib/molecules/MenuItem/MenuItem.d.ts
@@ -1,9 +1,10 @@
 import React from 'react';
 export type MenuItemProps = {
     children: React.ReactNode;
-    showToggleButton?: boolean;
-    toggleButton?: React.ReactNode;
     sx?: Record<string, any>;
+    startIcon?: React.ReactNode;
+    endIcon?: React.ReactNode;
+    component?: React.ReactNode;
 } & React.HTMLAttributes<HTMLDivElement>;
 declare const MenuItem: React.FC<MenuItemProps>;
 export default MenuItem;
--- src/molecules/MenuItem/MenuItem.tsx
+++ src/molecules/MenuItem/MenuItem.tsx
@@ -3,31 +3,41 @@ import styled, { css } from 'styled-components'
 
 export type MenuItemProps = {
   children: React.ReactNode
-  showToggleButton?: boolean
-  toggleButton?: React.ReactNode
   sx?: Record<string, any>
+  startIcon?: React.ReactNode
+  endIcon?: React.ReactNode
+  component?: React.ReactNode
 } & React.HTMLAttributes<HTMLDivElement>
 
-const StyledMenuItem = styled.div<MenuItemProps>`
-  background-color: #fff;
-  height: 100vh;
-  position: fixed;
-  left: 0;
-  width: 100%;
+const StyledMenuItem = styled.div<Pick<MenuItemProps, 'sx'>>`
+  display: flex;
+  align-items: center;
+  gap: 8px;
   padding: 8px 16px;
+  & > .menu-item-content {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 8px;
+    padding: 8px;
+  }
   ${({ sx }) => sx && css(sx)};
 `
 
 const MenuItem: React.FC<MenuItemProps> = ({
   children,
   sx,
-  showToggleButton = true,
-  toggleButton,
+  startIcon,
+  component,
+  endIcon,
   ...rest
 }: MenuItemProps) => {
   return (
     <StyledMenuItem sx={sx} {...rest}>
+      {component && component}
+      {startIcon && startIcon}
       {children}
+      {endIcon && endIcon}
     </StyledMenuItem>
   )
 }
