ebd6c8436e2f9ebc0d8c250747d40b680a171e5a
dnhieu1992 <dnhieu1992@gmail.com>
2024-04-27 10:55:46 +0700

    Fix bug MenuItem component

--- src/molecules/MenuItem/MenuItem.tsx
+++ src/molecules/MenuItem/MenuItem.tsx
@@ -3,41 +3,31 @@ import styled, { css } from 'styled-components'
 
 export type MenuItemProps = {
   children: React.ReactNode
+  showToggleButton?: boolean
+  toggleButton?: React.ReactNode
   sx?: Record<string, any>
-  startIcon?: React.ReactNode
-  endIcon?: React.ReactNode
-  component?: React.ReactNode
 } & React.HTMLAttributes<HTMLDivElement>
 
-const StyledMenuItem = styled.div<Pick<MenuItemProps, 'sx'>>`
-  display: flex;
-  align-items: center;
-  gap: 8px;
+const StyledMenuItem = styled.div<MenuItemProps>`
+  background-color: #fff;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  width: 100%;
   padding: 8px 16px;
-  & > .menu-item-content {
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    gap: 8px;
-    padding: 8px;
-  }
   ${({ sx }) => sx && css(sx)};
 `
 
 const MenuItem: React.FC<MenuItemProps> = ({
   children,
   sx,
-  startIcon,
-  component,
-  endIcon,
+  showToggleButton = true,
+  toggleButton,
   ...rest
 }: MenuItemProps) => {
   return (
     <StyledMenuItem sx={sx} {...rest}>
-      {component && component}
-      {startIcon && startIcon}
-      <span>{children}</span>
-      {endIcon && endIcon}
+      {children}
     </StyledMenuItem>
   )
 }
